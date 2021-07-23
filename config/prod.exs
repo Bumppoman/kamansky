4189f1037b8528c5630524d57d64d98fd34a6047
Brendon Stanton <brendonstanton@icloud.com>
2021-07-23 09:52:39 -0400

    Working toward SSL?

--- config/prod.exs
+++ config/prod.exs
@@ -13,6 +13,7 @@ config :kamansky, KamanskyWeb.Endpoint,
   url:
     [
       host: System.get_env("HOSTNAME") || "localhost",
+      port: 443,
       scheme: :https
     ],
   cache_static_manifest: "priv/static/cache_manifest.json"
