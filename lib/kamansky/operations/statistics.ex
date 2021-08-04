e80c1d920e2d40a5f7718178a9e6919e742a877c
Brendon Stanton <brendonstanton@icloud.com>
2021-08-04 19:06:22 -0400

    Begin the statistics module

--- lib/kamansky/operations/statistics.ex
+++ lib/kamansky/operations/statistics.ex
@@ -1,10 +1,12 @@
 defmodule Kamansky.Operations.Statistics do
+  import Kamansky.Helpers, only: [format_decimal_as_currency: 1]
+
   alias Kamansky.Sales.Orders
 
   @spec get_statistics(integer, integer) :: map
   def get_statistics(month, year) do
     %{
-      gross_sales: Orders.total_gross_profit(month: month, year: year)
+      gross_profit: Orders.total_gross_profit(month: month, year: year)
     }
   end
 end
