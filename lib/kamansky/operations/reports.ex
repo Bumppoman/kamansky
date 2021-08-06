e05db06270c3215d4e3cd28e95cf325875076e29
Brendon Stanton <brendonstanton@icloud.com>
2021-08-05 20:24:45 -0400

    Selling fees breakdown for report

--- lib/kamansky/operations/reports.ex
+++ lib/kamansky/operations/reports.ex
@@ -2,25 +2,31 @@ defmodule Kamansky.Operations.Reports do
   import Ecto.Query, warn: false
 
   alias Kamansky.Repo
-  alias Kamansky.Sales.Orders.Order
+  alias Kamansky.Stamps.Stamp
 
   @spec get_order_data(integer, integer) :: map
   def get_order_data(year, month) do
     with(
+      stamps_query <-
+        Stamp
+        |> join(:left, [s], l in assoc(s, :listing))
+        |> where([s, l], parent_as(:order).id == l.order_id)
+        |> select([s], %{stamp_cost: sum(s.cost + s.purchase_fees)}),
       base_data <-
-        from(o in "orders")
+        from(o in "orders", as: :order)
         |> order_for_year_and_month_query(year, month)
+        |> join(:left_lateral, [o], ss in subquery(stamps_query))
         |> select(
-          [o],
+          [o, ss],
           %{
-            ebay_sales: fragment(
+            ebay_gross_sales: fragment(
               "SUM(CASE WHEN ? IS NOT NULL THEN (? + ?) ELSE 0 END)",
               o.ebay_id,
               o.item_price,
               o.shipping_price
             ),
             ebay_selling_fees: fragment("SUM(CASE WHEN ? IS NOT NULL THEN ? ELSE 0 END)", o.ebay_id, o.selling_fees),
-            hipstamp_sales: fragment(
+            hipstamp_gross_sales: fragment(
               "SUM(CASE WHEN ? IS NOT NULL THEN (? + ?) ELSE 0 END)",
               o.hipstamp_id,
               o.item_price,
@@ -28,34 +34,41 @@ defmodule Kamansky.Operations.Reports do
             ),
             hipstamp_selling_fees: fragment("SUM(CASE WHEN ? IS NOT NULL THEN ? ELSE 0 END)", o.hipstamp_id, o.selling_fees),
             gross_sales: sum(fragment("? + ?", o.item_price, o.shipping_price)),
+            net_sales: sum(
+              fragment(
+                "(? + ?) - ? - ? - ?",
+                o.item_price,
+                o.shipping_price,
+                o.selling_fees,
+                o.shipping_cost,
+                ss.stamp_cost
+              )
+            ),
             selling_fees: sum(o.selling_fees),
             shipping_cost: sum(o.shipping_cost),
+            stamp_cost: sum(ss.stamp_cost)
           }
         )
         |> Repo.one(),
-      stamp_cost <- total_stamp_cost_for_year_and_month(year, month),
-      net_sales <- total_net_sales(
-        gross_sales: base_data.gross_sales,
-        selling_fees: base_data.selling_fees,
-        shipping_cost: base_data.shipping_cost,
-        stamp_cost: stamp_cost
-      ),
       calculated_data <-
         %{
-          ebay_sales_percentage: calculate_percentage(base_data.ebay_sales, base_data.gross_sales),
-          hipstamp_sales_percentage: calculate_percentage(base_data.hipstamp_sales, base_data.gross_sales),
-          net_sales: net_sales,
-          net_sales_percentage: calculate_percentage(net_sales, base_data.gross_sales),
+          ebay_sales_percentage: calculate_percentage(base_data.ebay_gross_sales, base_data.gross_sales),
+          ebay_selling_fees_percentage_of_gross:
+            calculate_percentage(base_data.ebay_selling_fees, base_data.ebay_gross_sales),
+          hipstamp_sales_percentage: calculate_percentage(base_data.hipstamp_gross_sales, base_data.gross_sales),
+          hipstamp_selling_fees_percentage_of_gross:
+            calculate_percentage(base_data.hipstamp_selling_fees, base_data.hipstamp_gross_sales),
+          net_sales_percentage: calculate_percentage(base_data.net_sales, base_data.gross_sales),
           selling_fees_percentage: calculate_percentage(base_data.selling_fees, base_data.gross_sales),
           shipping_cost_percentage: calculate_percentage(base_data.shipping_cost, base_data.gross_sales),
-          stamp_cost: stamp_cost,
-          stamp_cost_percentage: calculate_percentage(stamp_cost, base_data.gross_sales)
+          stamp_cost_percentage: calculate_percentage(base_data.stamp_cost, base_data.gross_sales)
         }
     ) do
       Map.merge(base_data, calculated_data)
     end
   end
 
+  @spec begin_and_end_date_for_year_and_month(integer, integer) :: {DateTime.t, DateTime.t}
   defp begin_and_end_date_for_year_and_month(year, month) do
     with(
       begin_date <-
@@ -74,6 +87,7 @@ defmodule Kamansky.Operations.Reports do
     end
   end
 
+  @spec calculate_percentage(Decimal.t, Decimal.t) :: integer
   defp calculate_percentage(numerator, denominator) do
     numerator
     |> Decimal.div(denominator)
@@ -82,6 +96,7 @@ defmodule Kamansky.Operations.Reports do
     |> Kernel.round()
   end
 
+  @spec order_for_year_and_month_query(Ecto.Queryable.t, integer, integer) :: Ecto.Query.t
   defp order_for_year_and_month_query(query, year, month) do
     with {begin_date, end_date} <- begin_and_end_date_for_year_and_month(year, month) do
       where(
@@ -89,32 +104,6 @@ defmodule Kamansky.Operations.Reports do
         [o],
         fragment("? BETWEEN ? AND ?", o.ordered_at, ^begin_date, ^end_date)
       )
-    end
-  end
-
-  defp total_net_sales(
-    gross_sales: gross_sales, selling_fees: selling_fees, shipping_cost: shipping_cost, stamp_cost: stamp_cost
-  ) do
-    Decimal.sub(gross_sales,
-      Decimal.add(
-        selling_fees,
-        Decimal.add(shipping_cost, stamp_cost)
-      )
-    )
-  end
-
-  defp total_stamp_cost_for_year_and_month(year, month) do
-    with(
-      order_subquery <-
-        Order
-        |> order_for_year_and_month_query(year, month)
-        |> select([o], o.id)
-    ) do
-      from(l in "listings")
-      |> where([l], l.order_id in subquery(order_subquery))
-      |> join(:left, [l], s in "stamps", on: l.stamp_id == s.id)
-      |> select([l, s], sum(fragment("? + ?", s.cost, s.purchase_fees)))
-      |> Repo.one()
     end
   end
 end
