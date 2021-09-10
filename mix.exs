d0cfc64da0bb6f60ff489df6f94d6c14980c937c
Brendon Stanton <brendonstanton@icloud.com>
2021-09-10 16:01:51 -0400

    Updating live_view

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule Kamansky.MixProject do
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.7"},
       {:postgrex, ">= 0.15.10"},
-      {:phoenix_live_view, "~> 0.16.1"},
+      {:phoenix_live_view, "~> 0.16"},
       {:floki, ">= 0.0.0", only: :test},
       {:phoenix_html, "~> 3.0.2"},
       {:phoenix_live_reload, "~> 1.3", only: :dev},
