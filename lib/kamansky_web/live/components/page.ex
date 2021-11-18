ac851d6db967c98781045c26f5b69e1603bd28d9
Brendon Stanton <brendonstanton@icloud.com>
2021-11-18 17:21:05 -0500

    Fixed a few issues.

--- lib/kamansky_web/live/components/page.ex
+++ lib/kamansky_web/live/components/page.ex
@@ -81,14 +81,14 @@ defmodule KamanskyWeb.Components.Page do
                 :phx-click="detail.action"
                 x-ref="success"
               >
-                <div id="kamansky-confirmation-modal-button-loading">
+                <div class="kamansky-button-loading">
                   <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                     <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                     <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                   </svg>
                   Processing
                 </div>
-                <span id="kamansky-confirmation-modal-button-normal" x-text="detail.title" />
+                <span class="kamansky-button-normal" x-text="detail.title" />
               </button>
               <button
                 type="button"
