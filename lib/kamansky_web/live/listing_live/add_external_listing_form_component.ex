4f7f030ba17a8e05e07f6530b1d054be68ec5968
Brendon Stanton <brendonstanton@icloud.com>
2021-12-19 11:25:44 -0500

    Dependency updates

--- lib/kamansky_web/live/listing_live/add_external_listing_form_component.ex
+++ lib/kamansky_web/live/listing_live/add_external_listing_form_component.ex
@@ -9,24 +9,29 @@ defmodule KamanskyWeb.ListingLive.AddExternalListingFormComponent do
 
   @impl true
   @spec handle_event(String.t, %{required(String.t) => any}, Phoenix.LiveView.Socket.t) :: {:noreply, Phoenix.LiveView.Socket.t}
-  def handle_event("validate", %{"external_listing" => external_listing_params}, socket) do
-    %EbayListing{}
-    |> Platforms.change_external_listing(external_listing_params)
-    |> Map.put(:action, :validate)
-    |> then(&assign(socket, :changeset, &1))
-    |> noreply()
+  def handle_event("validate", params, socket) do
+    with type <- socket.assigns.type do
+      type
+      |> empty_external_listing()
+      |> Platforms.change_external_listing(external_listing_params(type, params))
+      |> Map.put(:action, :validate)
+      |> then(&assign(socket, :changeset, &1))
+      |> noreply()
+    end
   end
 
-  def handle_event("submit", %{"external_listing" => external_listing_params}, socket) do
-    socket.assigns.listing
-    |> Kamansky.Services.Stamp.create_new_external_listing_for_existing_listing(socket.assigns.type, external_listing_params)
-    |> case do
-      {:ok, %EbayListing{listing_id: listing_id}} -> send self(), {:listing_listed_on_ebay, listing_id}
-      {:ok, %HipstampListing{listing_id: listing_id}} -> send self(), {:listing_listed_on_hipstamp, listing_id}
-      {:error, error} -> send self(), {:error, error}
-    end
+  def handle_event("submit", params, socket) do
+    with type <- socket.assigns.type do
+      socket.assigns.listing
+      |> Kamansky.Services.Stamp.create_new_external_listing_for_existing_listing(type, external_listing_params(type, params))
+      |> case do
+        {:ok, %EbayListing{listing_id: listing_id}} -> send self(), {:listing_listed_on_ebay, listing_id}
+        {:ok, %HipstampListing{listing_id: listing_id}} -> send self(), {:listing_listed_on_hipstamp, listing_id}
+        {:error, error} -> send self(), {:error, error}
+      end
 
-    noreply(socket)
+      noreply(socket)
+    end
   end
 
   @impl true
@@ -41,6 +46,14 @@ defmodule KamanskyWeb.ListingLive.AddExternalListingFormComponent do
       |> assign(:type, type)
     end
   end
+
+  @spec empty_external_listing(atom) :: EbayListing.t | HipstampListing.t
+  defp empty_external_listing(:ebay), do: %EbayListing{}
+  defp empty_external_listing(:hipstamp), do: %HipstampListing{}
+
+  @spec external_listing_params(atom, map) :: map
+  defp external_listing_params(:ebay, %{"ebay_listing" => ebay_listing_params}), do: ebay_listing_params
+  defp external_listing_params(:hipstamp, %{"hipstamp_listing" => hipstamp_listing_params}), do: hipstamp_listing_params
 
   @spec form_component(:ebay | :hipstamp) :: fun
   defp form_component(:ebay), do: &KamanskyWeb.ListingLive.Components.ebay_form/1
